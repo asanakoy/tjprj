ddbf7ed0d8cfc696ebf17e22e3cec14c516525cd
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-10-25 20:10:53 +0200

    add lsp/compute_sim

--- eval/image_getter.py
+++ eval/image_getter.py
@@ -3,7 +3,7 @@ import h5py
 import numpy as np
 import PIL
 from PIL import Image
-
+import scipy.misc
 
 class ImageGetterFromMat:
     """
@@ -38,6 +38,51 @@ class ImageGetterFromMat:
             batch = resized_batch
 
         batch = np.asarray(batch, dtype=np.float32)
+        if mean is not None:
+            batch -= np.tile(mean, (batch.shape[0], 1, 1, 1))
+        batch = batch[:, :, :, (2, 1, 0)]  # reorder channels RGB -> BGR
+        return batch
+
+
+class ImageGetterFromPaths:
+    """
+    Class for loading batches by index from disk by paths
+    """
+
+    def __init__(self, image_paths, im_shape):
+        """
+        :param image_paths: list of full pathes
+        :param im_shape: default im_shape to use when reading images
+        """
+        assert len(im_shape) == 2, 'im_shape must be of len 2: (h, w)!'
+        self.image_paths = image_paths
+        self.im_shape = im_shape
+
+    def total_num_images(self):
+        return len(self.image_paths)
+
+    def get_batch(self, indxs, resize_shape=None, mean=None):
+        """
+        Get batch by the indices of the images.
+        :param indxs: numeric indices of the images to include in the batch
+        :param resize_shape: resize images to this shape.
+                             If None, resize to self.im_shape.
+        :param mean: must be HxWxC RGB
+        :return: batch of images as np.array NxHxWxC with BGR channel order
+        """
+        assert len(resize_shape) == 2, 'resize_shape must be of len 2: (h, w)!'
+        assert mean is None or (len(mean.shape) == 3 and mean.shape[2] == 3)
+
+        if resize_shape is None:
+            resize_shape = self.im_shape
+
+        # NxHxWxC RGB matrix
+        batch = np.zeros((len(indxs),) + resize_shape + (3,), dtype=np.float32)
+        for i, image_idx in enumerate(indxs):
+            image = Image.open(self.image_paths[image_idx]).resize(resize_shape,
+                                                                   PIL.Image.ANTIALIAS)
+            batch[i, ...] = np.asarray(image, dtype=np.float32)
+
         if mean is not None:
             batch -= np.tile(mean, (batch.shape[0], 1, 1, 1))
         batch = batch[:, :, :, (2, 1, 0)]  # reorder channels RGB -> BGR
---
+++ eval/lsp/compute_sim.py
@@ -0,0 +1,50 @@
+import sys
+from os.path import join
+import glob
+import numpy as np
+
+from demo.train import get_num_classes
+from eval.image_getter import ImageGetterFromPaths
+import eval.features
+
+
+def main(argv):
+    if len(argv) == 0:
+        gpu_id = 0
+    else:
+        gpu_id = int(argv[0])
+
+    crops_dir = '/export/home/asanakoy/workspace/lsp/crops_227x227'
+    image_paths = [join(crops_dir, p) for p in glob.glob1(crops_dir, '*.png')]
+
+    indices_dir = '/export/home/mbautist/Desktop/workspace/cnn_similarities/MIL-CliqueCNN/clustering/LSP/iter_1/'
+    train_indices_path = join(indices_dir, 'train_indices.mat')
+    mean_path = join(indices_dir, 'mean.npy')
+    mean = np.load(mean_path)
+    num_classes = get_num_classes(train_indices_path)
+
+    iteration = 30000
+    init_model = '/export/home/asanakoy/workspace/lsp/cnn/checkpoint-{}'.format(iteration)
+    model_name = 'tf_0.1conv_1fc_{}it'.format(iteration)
+    im_shape = (227, 227)
+
+    params = {
+        'model_name': model_name,
+        'category': '',
+        'iter': iteration,
+        'layer_names': ['fc6'],
+        'image_getter': ImageGetterFromPaths(image_paths, im_shape),
+        'mean': None, #mean,
+        'im_shape': im_shape,
+        'batch_size': 256,
+        'num_classes': num_classes,
+        'snapshot_path': init_model,
+        'sim_output_dir': join(
+            '/export/home/asanakoy/workspace/lsp/sim/tf'),
+        'device_id': '/gpu:{}'.format(gpu_id)
+    }
+    eval.features.compute_sim_and_save(**params)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
