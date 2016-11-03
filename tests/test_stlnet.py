5aefc9bce29c60ca0f2051d88e65526adc7fbf50
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-11-03 02:31:45 +0100

    add STLNet

---
+++ tests/test_stlnet.py
@@ -0,0 +1,90 @@
+# Test STLNet
+# Copyright (c) 2016 Artsiom Sanakoyeu
+import numpy as np
+import time
+import os
+from scipy.misc import imread, imresize
+import tensorflow as tf
+import tfext.stlnet
+
+MODELS_DIR = '/export/home/asanakoy/workspace/tfprj/data'
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+
+
+def test_feed_forward(net, im_shape):
+    print 'test_feed_forward'
+    batch = np.zeros((5,) + im_shape, dtype=np.float)
+    for i in xrange(5):
+        im = imread(os.path.join(DATA_DIR, "{}.png".format(i + 1)))[:, :, :3]
+        im = imresize(im, im_shape[:2]).astype(np.float32)
+        im = im - np.mean(im)
+        batch[i, ...] = im
+    batch = batch[:, :, :, ::-1]  # MAKE BGR!
+
+    t = time.time()
+    output = net.sess.run(net.prob, feed_dict={'input/x:0': batch, 'input/is_phase_train:0': False})
+    for input_im_ind in range(output.shape[0]):
+        print 'Image {}:  {}'.format(input_im_ind, output[input_im_ind])
+    print time.time() - t
+
+
+if __name__ == '__main__':
+    params = {
+        'device_id': '/gpu:0',
+        'im_shape': (96, 96, 3),
+        'gpu_memory_fraction': None,
+        'random_init_type': tfext.stlnet.Stlnet.RandomInitType.XAVIER_GAUSSIAN
+    }
+
+    net = tfext.stlnet.Stlnet(**params)
+
+    with net.graph.as_default():
+        cross_entropy = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(net.fc13, net.y_gt))
+        update_ops = net.graph.get_collection(tf.GraphKeys.UPDATE_OPS)
+        assert len(update_ops) > 0
+        with tf.control_dependencies(update_ops):
+            train_step = tf.train.AdagradOptimizer(learning_rate=1e-3,
+                                                   initial_accumulator_value=0.00001).minimize(cross_entropy, global_step=net.global_iter_counter)
+            # train_step = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cross_entropy,
+            #                                                                      global_step=net.global_iter_counter)
+        correct_prediction = tf.equal(tf.cast(tf.argmax(net.prob, 1), tf.int32), net.y_gt)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    net.sess.run(tf.initialize_all_variables())
+
+    test_feed_forward(net, params['im_shape'])
+
+    batch = np.zeros((5,) + params['im_shape'], dtype=np.float)
+    for i in xrange(5):
+        im = imread(os.path.join(DATA_DIR, "{}.png".format(i + 1)))[:, :, :3]
+        im = imresize(im, params['im_shape'][:2]).astype(np.float32)
+        im = im - np.mean(im)
+        batch[i, ...] = im
+    batch = batch[:, :, :, ::-1]
+
+    for i in range(750):
+        # batch = np.random.random((5, 96, 96, 3))
+        y = [0, 1, 5, 9, 3]
+
+        probs, train_accuracy, _, global_iter = net.sess.run([net.prob, accuracy, train_step, net.global_iter_counter],
+                                                              feed_dict={net.x: batch,
+                                                                         net.y_gt: y,
+                                                                         net.dropout_keep_prob: 0.5,
+                                                                         net.is_phase_train: True})
+        test_probs, test_accuracy, global_iter = net.sess.run([net.prob, accuracy, net.global_iter_counter],
+                                                   feed_dict={net.x: batch,
+                                                   net.y_gt: y,
+                                                   net.dropout_keep_prob: 1.0,
+                                                   net.is_phase_train: False})
+        np.set_printoptions(precision=2)
+        print "step {}, training accuracy {:.2f}: {}".format(global_iter, train_accuracy, probs[:, 0])
+        print "step {}, test accuracy     {:.2f}: {}".format(global_iter, test_accuracy, test_probs[:, 0])
+        if i % 600 == 0:
+            net.reset_fc13()
+
+    saver = tf.train.Saver()
+    checkpoint_prefix = os.path.expanduser('~/tmp/tmp-checkpoint-tensorflow-test')
+    saver.save(net.sess, checkpoint_prefix, global_step=1, write_meta_graph=False)
+    net.restore_from_snapshot(checkpoint_prefix + '-1', 13)
+    net.reset_fc13()
---
+++ tfext/stlnet.py
@@ -0,0 +1,250 @@
+################################################################################
+# Deep model for STL-10 training
+# Copyright (c) 2016 Artsiom Sanakoyeu
+################################################################################
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.layers as tflayers
+
+
+class Stlnet(object):
+    """
+    STLNet
+    WARNING! You should feed images in HxWxC BGR format!
+    """
+
+    class RandomInitType:
+        GAUSSIAN = 0,
+        XAVIER_UNIFORM = 1,
+        XAVIER_GAUSSIAN = 2
+
+    def __init__(self,
+                 im_shape=(96, 96, 3),
+                 device_id='/gpu:0',
+                 random_init_type=RandomInitType.GAUSSIAN,
+                 gpu_memory_fraction=None):
+        """
+         Args:
+          init_model: dict containing network weights, or a string with path to .np file with the dict,
+            if is None then init using random weights and biases
+          num_classes: number of output classes
+          gpu_memory_fraction: Fraction on the max GPU memory to allocate for process needs.
+            Allow auto growth if None (can take up to the totality of the memory).
+        :return:
+        """
+        self.input_shape = im_shape
+        self.device_id = device_id
+        self.random_init_type = random_init_type
+        self.batch_norm_decay = 0.99
+
+        if len(self.input_shape) == 2:
+            self.input_shape += (3,)
+
+        assert len(self.input_shape) == 3
+
+        self.global_iter_counter = tf.Variable(0, name='global_iter_counter', trainable=False)
+        with tf.variable_scope('input'):
+            self.x = tf.placeholder(tf.float32, (None,) + self.input_shape, name='x')
+            self.y_gt = tf.placeholder(tf.int32, shape=(None,), name='y_gt')
+            self.is_phase_train = tf.placeholder(tf.bool, shape=tuple(), name='is_phase_train')
+            self.dropout_keep_prob = tf.placeholder_with_default(1.0, tuple(),
+                                                                 name='dropout_keep_prob')
+
+        with tf.device(self.device_id):
+            conv1 = self.conv_relu(self.x, kernel_size=5,
+                                   kernels_num=64, stride=2,
+                                   name='conv1')
+            conv2 = self.conv_relu(conv1, kernel_size=1,
+                                   kernels_num=160, stride=1,
+                                   name='conv2')
+            conv3 = self.conv_relu(conv2, kernel_size=1,
+                                   kernels_num=96, stride=1,
+                                   name='conv3')
+
+            maxpool3 = tf.nn.max_pool(conv3,
+                                      ksize=[1, 3, 3, 1],
+                                      strides=[1, 2, 2, 1],
+                                      padding='VALID',
+                                      name='maxpool3')
+
+            conv4 = self.conv_relu(maxpool3, kernel_size=5,
+                                   kernels_num=192, stride=2,
+                                   name='conv4')
+            conv5 = self.conv_relu(conv4, kernel_size=1,
+                                   kernels_num=192, stride=1,
+                                   name='conv5')
+            conv6 = self.conv_relu(conv5, kernel_size=1,
+                                   kernels_num=192, stride=1,
+                                   name='conv6')
+
+            maxpool6 = tf.nn.max_pool(conv6,
+                                      ksize=[1, 3, 3, 1],
+                                      strides=[1, 2, 2, 1],
+                                      padding='VALID',
+                                      name='maxpool6')
+
+            conv7 = self.conv_relu(maxpool6, kernel_size=3,
+                                   kernels_num=192, stride=1,
+                                   name='conv7')
+            conv8 = self.conv_relu(conv7, kernel_size=1,
+                                   kernels_num=192, stride=1,
+                                   name='conv8')
+            conv9 = self.conv_relu(conv8, kernel_size=1,
+                                   kernels_num=192, stride=1,
+                                   name='conv9')
+
+            conv10 = self.conv_relu(conv9, kernel_size=3,
+                                    kernels_num=256, stride=1,
+                                    batch_norm=False,
+                                    name='conv10')
+
+            maxpool10 = tf.nn.max_pool(conv10,
+                                       ksize=[1, 3, 3, 1],
+                                       strides=[1, 2, 2, 1],
+                                       padding='VALID',
+                                       name='maxpool10')
+            dropout10 = tf.nn.dropout(maxpool10, self.dropout_keep_prob, name='dropout10')
+
+            conv11 = self.conv_relu(dropout10, kernel_size=3,
+                                    kernels_num=128, stride=1,
+                                    batch_norm=False,
+                                    name='conv11')
+            dropout11 = tf.nn.dropout(conv11, self.dropout_keep_prob, name='dropout11')
+
+            self.fc12, self.fc12_relu = self.fc_relu(dropout11,
+                                                     num_outputs=512,
+                                                     relu=True,
+                                                     weight_std=0.005, bias_init_value=0.1,
+                                                     name='fc12')
+            dropout12 = tf.nn.dropout(self.fc12_relu, self.dropout_keep_prob, name='dropout12')
+
+            self.fc13 = self.fc_relu(dropout12,
+                                     num_outputs=10,
+                                     relu=False,
+                                     weight_std=0.01, bias_init_value=0.0,
+                                     name='fc13')[0]
+
+            with tf.variable_scope('output'):
+                self.prob = tf.nn.softmax(self.fc13, name='prob')
+
+            fc13_w = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "fc13/weight:0")[0]
+            fc13_b = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "fc13/bias:0")[0]
+            self.reset_fc13_op = tf.initialize_variables([fc13_w, fc13_b], name='reset_fc13')
+
+        self.graph = tf.get_default_graph()
+        config = tf.ConfigProto(log_device_placement=False,
+                                allow_soft_placement=True)
+        # please do not use the totality of the GPU memory.
+        if gpu_memory_fraction is None:
+            config.gpu_options.allow_growth = True
+        else:
+            config.gpu_options.per_process_gpu_memory_fraction = gpu_memory_fraction
+        self.sess = tf.Session(config=config)
+
+    def restore_from_snapshot(self, snapshot_path, num_layers):
+        """
+        :param snapshot_path: path to the snapshot file
+        :param num_layers: number layers to restore from the snapshot
+                            (conv1 is the #1, fc8 is the #8)
+        :param restore_iter_counter: if True restore global_iter_counter from the snapshot
+
+        WARNING! A call of sess.run(tf.initialize_all_variables()) after restoring from snapshot
+                 will overwrite all variables and set them to initial state.
+                 Call restore_from_snapshot() only after sess.run(tf.initialize_all_variables())!
+        """
+        if num_layers > 13 or num_layers < 12:
+            raise ValueError('You can restore only 12 or 13 layers.')
+        if num_layers == 0:
+            return
+        saver = tf.train.Saver()
+        saver.restore(self.sess, snapshot_path)
+        if num_layers == 12:
+            self.reset_fc13()
+
+    def reset_fc13(self):
+        print 'Resetting fc13 to random'
+        self.sess.run(self.reset_fc13_op)
+
+    def get_conv_weights(self, kernel_size, num_input_channels, kernels_num,
+                         weight_std=0.01, bias_init_value=0.1):
+        w = self.random_weight_variable((kernel_size, kernel_size,
+                                         num_input_channels,
+                                         kernels_num),
+                                        stddev=weight_std)
+        b = self.random_bias_variable((kernels_num,), value=bias_init_value)
+        return w, b
+
+    def get_fc_weights(self, num_inputs, num_outputs, weight_std=0.005, bias_init_value=0.1):
+        w = self.random_weight_variable((num_inputs, num_outputs), stddev=weight_std)
+        b = self.random_bias_variable((num_outputs,), value=bias_init_value)
+        return w, b
+
+    def random_weight_variable(self, shape, stddev=0.01):
+        """
+        stddev is used only for RandomInitType.GAUSSIAN
+        """
+        if self.random_init_type == Stlnet.RandomInitType.GAUSSIAN:
+            initial = tf.truncated_normal(shape, stddev=stddev)
+            return tf.Variable(initial, name='weight')
+        elif self.random_init_type == Stlnet.RandomInitType.XAVIER_GAUSSIAN:
+            return tf.get_variable("weight", shape=shape,
+                                   initializer=tf.contrib.layers.xavier_initializer(
+                                       uniform=False))
+        elif self.random_init_type == Stlnet.RandomInitType.XAVIER_UNIFORM:
+            return tf.get_variable("weight", shape=shape,
+                                   initializer=tf.contrib.layers.xavier_initializer(
+                                       uniform=True))
+        else:
+            raise ValueError('Unknown random_init_type')
+
+    @staticmethod
+    def random_bias_variable(shape, value=0.1):
+        initial = tf.constant(value, shape=shape)
+        return tf.Variable(initial, name='bias')
+
+    @staticmethod
+    def conv(input_tensor, kernel, biases, stride, padding="VALID"):
+        conv = tf.nn.conv2d(input_tensor, kernel, [1, stride, stride, 1], padding=padding)
+        # TODO: no need to reshape?
+        return tf.reshape(tf.nn.bias_add(conv, biases), [-1] + conv.get_shape().as_list()[1:],
+                          name='conv')
+
+    def conv_relu(self, input_tensor, kernel_size, kernels_num, stride, batch_norm=True,
+                  name=None):
+        with tf.variable_scope(name) as scope:
+            num_input_channels = int(input_tensor.get_shape()[3])
+            w, b = self.get_conv_weights(kernel_size, num_input_channels, kernels_num)
+            conv = Stlnet.conv(input_tensor, w, b, stride, padding="SAME")
+            if batch_norm:
+                conv = tf.cond(self.is_phase_train,
+                               lambda: tflayers.batch_norm(conv,
+                                                           decay=self.batch_norm_decay,
+                                                           is_training=True,
+                                                           trainable=False,
+                                                           reuse=None,
+                                                           scope=scope),
+                               lambda: tflayers.batch_norm(conv,
+                                                           decay=self.batch_norm_decay,
+                                                           is_training=False,
+                                                           trainable=False,
+                                                           reuse=True,
+                                                           scope=scope))
+            conv = tf.nn.relu(conv, name=name)
+        return conv
+
+    def fc_relu(self, input_tensor, num_outputs, relu=False, weight_std=0.005,
+                bias_init_value=0.1, name=None):
+        with tf.variable_scope(name):
+            num_inputs = int(np.prod(input_tensor.get_shape()[1:]))
+            w, b = self.get_fc_weights(num_inputs, num_outputs,
+                                       weight_std=weight_std,
+                                       bias_init_value=bias_init_value)
+            fc_relu = None
+
+            input_tensor_reshaped = tf.reshape(input_tensor, [-1, num_inputs])
+            if relu:
+                fc = tf.add(tf.matmul(input_tensor_reshaped, w), b, name='fc')
+                fc_relu = tf.nn.relu(fc, name=name)
+            else:
+                fc = tf.add(tf.matmul(input_tensor_reshaped, w), b, name=name)
+        return fc, fc_relu
