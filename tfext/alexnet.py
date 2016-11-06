1bf5ad925c8e4f44418011515d1f7ec46f320202
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-11-06 23:39:20 +0100

    update alexnet

--- tfext/alexnet.py
+++ tfext/alexnet.py
@@ -33,7 +33,8 @@ class Alexnet(object):
 
     class RandomInitType:
         GAUSSIAN = 0,
-        XAVIER = 1
+        XAVIER_UNIFORM = 1,
+        XAVIER_GAUSSIAN = 2
 
     def __init__(self, init_model=None, num_classes=1,
                  im_shape=(227, 227, 3), device_id='/gpu:0', num_layers_to_init=8,
@@ -53,7 +54,7 @@ class Alexnet(object):
         self.device_id = device_id
         self.num_layers_to_init = num_layers_to_init
         self.random_init_type = random_init_type
-        tr_vars = dict()
+        self.trainable_vars = None
 
         if len(self.input_shape) == 2:
             self.input_shape += (3,)
@@ -77,6 +78,21 @@ class Alexnet(object):
             self.y_gt = tf.placeholder(tf.int32, shape=(None,), name='y_gt')
             self.is_phase_train = tf.placeholder(tf.bool, shape=tuple(), name='is_phase_train')
 
+        self.__create_architecture(net_data, use_batch_norm)
+
+        self.graph = tf.get_default_graph()
+        config = tf.ConfigProto(log_device_placement=False,
+                                allow_soft_placement=True)
+        # please do not use the totality of the GPU memory.
+        if gpu_memory_fraction is None:
+            config.gpu_options.allow_growth = True
+        else:
+            config.gpu_options.per_process_gpu_memory_fraction = gpu_memory_fraction
+        self.sess = tf.Session(config=config)
+
+    def __create_architecture(self, net_data, use_batch_norm):
+        print 'Alexnet::__create_architecture()'
+        tr_vars = dict()
         with tf.device(self.device_id):
             layer_index = 0
             # conv(11, 11, 96, 4, 4, padding='VALID', name='conv1')
@@ -247,8 +263,8 @@ class Alexnet(object):
                 if use_batch_norm:
                     print 'Using batch_norm after FC6'
                     self.fc6_bn = tflayers.batch_norm(self.fc6, decay=0.999,
-                                        is_training=self.is_phase_train,
-                                        trainable=False)
+                                                      is_training=self.is_phase_train,
+                                                      trainable=False)
                     out = self.fc6_bn
                 else:
                     out = self.fc6
@@ -258,7 +274,6 @@ class Alexnet(object):
                 self.fc6_keep_prob = tf.placeholder_with_default(1.0, tuple(),
                                                                  name='keep_prob_pl')
                 fc6_dropout = tf.nn.dropout(self.fc6_relu, self.fc6_keep_prob, name='dropout')
-
 
             # fc(4096, name='fc7')
             with tf.variable_scope('fc7'):
@@ -282,16 +297,16 @@ class Alexnet(object):
 
                 self.fc7_keep_prob = tf.placeholder_with_default(1.0, tuple(),
                                                                  name='keep_prob_pl')
-                fc7_dropout = tf.nn.dropout(self.fc7_relu, self.fc7_keep_prob, name='dropout')
+                self.fc7_dropout = tf.nn.dropout(self.fc7_relu, self.fc7_keep_prob, name='dropout')
 
             # fc(num_classes, relu=False, name='fc8')
             with tf.variable_scope('fc8'):
-                num_inputs = int(fc7_dropout.get_shape()[1])
+                num_inputs = int(self.fc7_dropout.get_shape()[1])
                 num_outputs = self.num_classes
                 tr_vars['fc8w'], tr_vars['fc8b'] = \
                     self.get_fc_weights(layer_index, net_data, num_inputs, num_outputs)
                 layer_index += 1
-                self.fc8 = tf.add(tf.matmul(fc7_dropout, tr_vars['fc8w']), tr_vars['fc8b'],
+                self.fc8 = tf.add(tf.matmul(self.fc7_dropout, tr_vars['fc8w']), tr_vars['fc8b'],
                                   name='fc')
                 assert self.fc8.get_shape()[1] == self.num_classes, \
                     '{} != {}'.format(self.fc8.get_shape()[1], self.num_classes)
@@ -299,17 +314,7 @@ class Alexnet(object):
             self.logits = self.fc8
             with tf.variable_scope('output'):
                 self.prob = tf.nn.softmax(self.fc8, name='prob')
-
-        self.graph = tf.get_default_graph()
         self.trainable_vars = tr_vars
-        config = tf.ConfigProto(log_device_placement=False,
-                                allow_soft_placement=True)
-        # please do not use the totality of the GPU memory.
-        if gpu_memory_fraction is None:
-            config.gpu_options.allow_growth = True
-        else:
-            config.gpu_options.per_process_gpu_memory_fraction = gpu_memory_fraction
-        self.sess = tf.Session(config=config)
 
     def restore_from_snapshot(self, snapshot_path, num_layers, restore_iter_counter=False):
         """
@@ -346,10 +351,10 @@ class Alexnet(object):
 
         l_name = layer_names[layer_index]
         if net_data is not None and layer_index < self.num_layers_to_init:
-            assert net_data[l_name][0].shape == (kernel_height, kernel_width,
+            assert net_data[l_name]['weights'].shape == (kernel_height, kernel_width,
                                                  num_input_channels,
                                                  kernels_num)
-            assert net_data[l_name][1].shape == (kernels_num,)
+            assert net_data[l_name]['biases'].shape == (kernels_num,)
 
         if layer_index >= self.num_layers_to_init or net_data is None:
             print 'Initializing {} with random'.format(l_name)
@@ -359,38 +364,58 @@ class Alexnet(object):
                                             stddev=wights_std[layer_index])
             b = self.random_bias_variable((kernels_num,), value=bias_init_values[layer_index])
         else:
-            w = tf.Variable(net_data[l_name][0], name='weight')
-            b = tf.Variable(net_data[l_name][1], name='bias')
+            w = tf.Variable(net_data[l_name]['weights'], name='weight')
+            b = tf.Variable(net_data[l_name]['biases'], name='bias')
         return w, b
 
-    def get_fc_weights(self, layer_index, net_data, num_inputs, num_outputs):
-        layer_names = ['conv{}'.format(i) for i in xrange(1, 6)] + \
-                      ['fc{}'.format(i) for i in xrange(6, 9)]
-        wights_std = [0.01] * 5 + [0.005, 0.005, 0.01]
-        bias_init_values = [0.0, 0.1, 0.0, 0.1, 0.1, 0.1, 0.1, 0.0]
-        l_name = layer_names[layer_index]
+    def get_fc_weights(self, layer_index, net_data, num_inputs, num_outputs,
+                       wights_std=None,
+                       bias_init_value=None):
+        if layer_index <= 8:
+            if wights_std is not None or bias_init_value is not None:
+                raise ValueError('std and bias must be None for layers 1..8, they are set up automatically')
+            layer_names = ['conv{}'.format(i) for i in xrange(1, 6)] + \
+                          ['fc{}'.format(i) for i in xrange(6, 9)]
+            wights_stds = [0.01] * 5 + [0.005, 0.005, 0.01]
+            bias_init_values = [0.0, 0.1, 0.0, 0.1, 0.1, 0.1, 0.1, 0.0]
+            l_name = layer_names[layer_index]
+
+            wights_std = wights_stds[layer_index]
+            bias_init_value = bias_init_values[layer_index]
+        else:
+            l_name = 'layer {}'.format(layer_index)
+            if wights_std is None or bias_init_value is None:
+                raise ValueError('std and bias must be provided for all layers beyond 1..8')
+
         if net_data is not None and layer_index < self.num_layers_to_init:
-            assert net_data[l_name][0].shape == (num_inputs, num_outputs)
-            assert net_data[l_name][1].shape == (num_outputs,)
+            assert net_data[l_name]['weights'].shape == (num_inputs, num_outputs)
+            assert net_data[l_name]['biases'].shape == (num_outputs,)
 
         if layer_index >= self.num_layers_to_init or net_data is None:
             print 'Initializing {} with random'.format(l_name)
             w = self.random_weight_variable((num_inputs, num_outputs),
-                                            stddev=wights_std[layer_index])
-            b = self.random_bias_variable((num_outputs,), value=bias_init_values[layer_index])
+                                            stddev=wights_std)
+            b = self.random_bias_variable((num_outputs,), value=bias_init_value)
         else:
-            w = tf.Variable(net_data[l_name][0], name='weight')
-            b = tf.Variable(net_data[l_name][1], name='bias')
+            w = tf.Variable(net_data[l_name]['weights'], name='weight')
+            b = tf.Variable(net_data[l_name]['biases'], name='bias')
         return w, b
 
     def random_weight_variable(self, shape, stddev=0.01):
+        """
+        stddev is used only for RandomInitType.GAUSSIAN
+        """
         if self.random_init_type == Alexnet.RandomInitType.GAUSSIAN:
             initial = tf.truncated_normal(shape, stddev=stddev)
             return tf.Variable(initial, name='weight')
-        elif self.random_init_type == Alexnet.RandomInitType.XAVIER:
+        elif self.random_init_type == Alexnet.RandomInitType.XAVIER_GAUSSIAN:
             return tf.get_variable("weight", shape=shape,
                                    initializer=tf.contrib.layers.xavier_initializer(
                                        uniform=False))
+        elif self.random_init_type == Alexnet.RandomInitType.XAVIER_UNIFORM:
+            return tf.get_variable("weight", shape=shape,
+                                   initializer=tf.contrib.layers.xavier_initializer(
+                                       uniform=True))
         else:
             raise ValueError('Unknown random_init_type')
 
