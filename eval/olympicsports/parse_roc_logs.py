d3c8604088c4c08149b45e4d49e98f0c214e3da1
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-11-23 16:42:56 +0100

    add parse_roc_logs script

---
+++ eval/olympicsports/parse_roc_logs.py
@@ -0,0 +1,56 @@
+import glob
+from os.path import join
+import re
+from collections import defaultdict
+
+if __name__ == '__main__':
+    dir_path = '/export/home/asanakoy/workspace/OlympicSports/cnn/sufflelearn_shufflelearnnet_eval_nomean'
+
+    ALL_CATEGORIES = [
+        'basketball_layup',
+        'bowling',
+        'clean_and_jerk',
+        'discus_throw',
+        'diving_platform_10m',
+        'diving_springboard_3m',
+        'hammer_throw',
+        'high_jump',
+        'javelin_throw',
+        'long_jump',
+        'pole_vault',
+        'shot_put',
+        'snatch',
+        'tennis_serve',
+        'triple_jump',
+        'vault']
+
+    LAYERS = ['maxpool5', 'fc6', 'fc7', 'fc8']
+    results = defaultdict(dict)
+    for cat in ALL_CATEGORIES:
+        print cat
+        for layer_name in LAYERS:
+            filenames = sorted(glob.glob1(join(dir_path, cat), layer_name + '_*'))
+            print filenames
+            if len(filenames) != 0:
+                matches = re.match(layer_name + r'_((?:[01][.])?[0-9]+)_iter-\d+', filenames[-1])
+                # matches = re.match(r'((?:[01][.])?[0-9]+)_' + layer_name + r'_checkpoint-\d+', filenames[-1])
+            else:
+                matches = None
+            if matches is not None:
+                auc = float(matches.groups()[0])
+                print cat, layer_name, auc
+                results[cat][layer_name] = auc
+            else:
+                print cat, layer_name, '---'
+
+        print '=========', cat, '==========='
+        print sorted(results[cat].items())
+        print '============================='
+
+    for layer_name in LAYERS:
+        print '=========', layer_name, '==========='
+        for cat in ALL_CATEGORIES:
+            if cat not in results or layer_name not in results[cat]:
+                print cat
+            else:
+                print '{}\t {:.3f}'.format(cat, results[cat][layer_name])
