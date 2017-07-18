bfe49de3ebc426df3cbc30f540c3e9034c4a77b4
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2017-07-18 17:14:50 +0200

    upd roc_auc

--- eval/olympicsports/roc/roc_auc.py
+++ eval/olympicsports/roc/roc_auc.py
@@ -88,10 +88,46 @@ def compute_interpolated_roc_auc(labels_dict, false_pos_rate_list, true_pos_rate
     return interp_roc_auc
 
 
-def compute_roc(d, sim):
-    stacked_sim_matrix = np.stack([sim['simMatrix'], sim['simMatrix_flip']], axis=2)
+# def compute_matlab_roc_auc(labels_dict, false_pos_rate_list, true_pos_rate_list):
+#     """
+#     Average results: interpolation for all anchors at 101 grid points.
+#     Then get joined ROC Curve by averaging interpolated values at grid points.
+#     Args:
+#         labels_dict: dict of labels
+#         false_pos_rate_list: i-th element is the list of false positive rates for the i-th anchor
+#         true_pos_rate_list: i-th element is the list of true positive rates for the i-th anchor
+#
+#     Return: joined ROC AUC
+#     """
+#     if len(false_pos_rate_list) != len(true_pos_rate_list):
+#         raise ValueError('fpr and tpr lists must be of the same size')
+#
+#     grid_x = np.linspace(0, 1, num=100, endpoint=True)
+#     assert len(grid_x) == 100
+#     # grid_x = np.unique(np.hstack(false_pos_rate_list))
+#     grid_y = np.zeros((len(labels_dict['anchors']), len(grid_x)))
+#     for i in xrange(len(labels_dict['anchors'])):
+#         unique_fpr, unique_idxs = np.unique(false_pos_rate_list[i], return_index=True)
+#         func = scipy.interpolate.interp1d(unique_fpr,
+#                                           true_pos_rate_list[i][unique_idxs],
+#                                           kind='linear', bounds_error=True)
+#         grid_y[i][...] = func(grid_x)
+#     mean_y = np.mean(grid_y, axis=0)
+#     interp_roc_auc = sklm.auc(grid_x, mean_y, reorder=True)
+#     return interp_roc_auc
+
+
+def compute_roc(d, sim,
+                old_field_names=False,
+                is_matlab_metric=False):
+    # TODO: change keys dependding on flag old_field_names
+    # TODO: make it possible to compute old matlab metric
+    assert not old_field_names
+    assert not is_matlab_metric
+    matrix_flipped_key = 'simMatrix_flip'
+    stacked_sim_matrix = np.stack([sim['simMatrix'], sim[matrix_flipped_key]], axis=2)
     assert stacked_sim_matrix.ndim == 3 and stacked_sim_matrix.shape[2] == 2
-    assert stacked_sim_matrix[0, 0, 0] > stacked_sim_matrix[0, 0, 1]
+    # assert stacked_sim_matrix[0, 0, 0] > stacked_sim_matrix[0, 0, 1]
 
     roc_auc_list = list()
     false_pos_rate_list = list()
@@ -106,10 +142,27 @@ def compute_roc(d, sim):
         true_pos_rate_list.append(tpr)
     avg_roc_auc = np.mean(roc_auc_list)
     interp_roc_auc = compute_interpolated_roc_auc(d, false_pos_rate_list, true_pos_rate_list)
+    # matlab_roc_auc = compute_matlab_roc_auc(d, false_pos_rate_list, true_pos_rate_list)
+    # print 'Matlab style ROC AUC:', matlab_roc_auc
     return avg_roc_auc, interp_roc_auc, roc_auc_list
 
 
-def compute_roc_auc_from_sim(argv, path_sim_matrix=None, is_quiet=False):
+def compute_roc_auc_from_sim(argv, path_sim_matrix=None,
+                             old_field_names=False,
+                             is_matlab_metric=False,
+                             is_quiet=False):
+    """
+
+    :param argv:
+    :param path_sim_matrix:
+    :param old_field_names: is mat file with old field names like: simMatrix_flip ?
+    :param is_matlab_metric: compute ROC in a way as our old matlab would compute (a bit incorrect)
+    :param is_quiet:
+    :return:
+    """
+    assert not old_field_names # not implemented
+    assert not is_matlab_metric
+
     if len(argv) == 0:
         category = 'long_jump'
     else:
@@ -137,7 +190,10 @@ def compute_roc_auc_from_sim(argv, path_sim_matrix=None, is_quiet=False):
     with h5py.File(labels_path, mode='r') as f:
         d = covert_labels_to_dict(f)
 
-    avg_roc_auc, interp_roc_auc, roc_auc_list = compute_roc(d, sim)
+    avg_roc_auc, interp_roc_auc, roc_auc_list = compute_roc(d, sim,
+                                                            old_field_names=old_field_names,
+                                                            is_matlab_metric=is_matlab_metric,
+                                                            )
     print '{} n_acnhors: {} mean_ROC_AUC: {:.3f} interp_ROC_AUC: {:.3f}'.format(category, len(
         d['anchors']), avg_roc_auc, interp_roc_auc)
     return interp_roc_auc
@@ -162,18 +218,28 @@ def run_all_cat():
         'triple_jump',
         'vault']
     categories = sorted(categories)
+    aucs = dict()
     for cat in categories:
         try:
-            path_sim_matrix = '/export/home/asanakoy/workspace/OlympicSports/sim/tf/{0}/simMatrix_{0}_tf_0.1conv_1fc_iter_20000_fc7_zscores.mat'.format(cat)
+            # path_sim_matrix = '/export/home/asanakoy/workspace/OlympicSports/sim/tf/{0}/simMatrix_{0}_tf_0.1conv_1fc_iter_20000_fc7_zscores.mat'.format(cat)
             # path_sim_matrix = '/export/home/mbautist/Desktop/workspace/cnn_similarities/compute_similarities/sim_matrices/imagenet/simMatrix_' + cat + '_imagenet-alexnet_iter_0_fc7.mat'
-            compute_roc_auc_from_sim([cat], path_sim_matrix=path_sim_matrix, is_quiet=True)
+            # path_sim_matrix = '/export/home/asanakoy/workspace/OlympicSports/sim/sim_matrices_miguel/' \
+            #                   'CliqueCNN/simMatrix_{0}_{0}_LR_0.001_M_0.9_BS_128_iter_20000_fc7_prerelu.mat'.format(cat)
+            # path_sim_matrix = '/export/home/asanakoy/workspace/OlympicSports/sim/sim_matrices_miguel/' \
+            #                   'NN-CNN/simMatrix_{0}_negExp_exemplarnns_{0}_LR_0.001_M_0.9_BS_128_iter_1000_fc7_prerelu.mat'.format(cat)
+            roc_auc = compute_roc_auc_from_sim([cat], path_sim_matrix=path_sim_matrix, is_quiet=True)
+            aucs[cat] = roc_auc
         except IOError as e:
             print e
             print cat
+    print 'Average ROC AUC:', np.mean(aucs.values())
 
 
 if __name__ == '__main__':
+    print 'refined'
     run_all_cat()
+    # compute_roc_auc_from_sim(['long_jump'],
+    #                          path_sim_matrix='/export/home/asanakoy/workspace/OlympicSports/sim/sim_matrices_miguel/CliqueCNN/simMatrix_correct_long_jump_cliqueCNN_long_jump_LR_0.001_M_0.9_BS_128_iter_20000_fc7_prerelu.mat')
     # compute_roc_auc_from_sim(['long_jump'], path_sim_matrix='/export/home/mbautist/Desktop/long_jump/simMatrix_long_jump_tf_0.1conv_1fc_growingiter2_iter_20000_fc7_zscores.mat')
     # compute_roc_auc_from_sim(['discus_throw'], path_sim_matrix='/export/home/asanakoy/workspace/OlympicSports/sim/tf/discus_throw/simMatrix_discus_throw_1_rounds_3_rounds_20k_aug_fc7_zscores.mat')
     # compute_roc_auc_from_sim(['long_jump'],
